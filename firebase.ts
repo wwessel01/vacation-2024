2350fe1484a169ee2b872e4791fcc0aa57c80000
Wessel van Tilburg <wessel.vantilburg@iodigital.com>
2024-02-02 08:35:09 +0100

    add base realtime functions

--- firebase.ts
+++ firebase.ts
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "vakantie-2024.firebaseapp.com",
@@ -7,4 +10,5 @@ const firebaseConfig = {
   appId: "1:1010816764772:web:cf9d52651d76c74f5c0871",
 };
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
--- src/routes/host.tsx
+++ src/routes/host.tsx
@@ -1,7 +1,31 @@
+import { collection } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { firestore } from "../../firebase.ts";
+
 export default function Host() {
+  const [value, loading, error] = useCollection(
+    collection(firestore, "messages")
+  );
   return (
     <div>
       <h1>Host page!</h1>
+      <div>
+        <h2>Chat</h2>
+
+        {loading && <p>Loading...</p>}
+
+        {error && <p>Error: {error.message}</p>}
+
+        {value && (
+          <div>
+            {value.docs.map((doc) => (
+              <div key={doc.id}>
+                <p>{JSON.stringify(doc.data())}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
