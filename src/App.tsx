0f3c364b41e3942c2dd595e9a7ba747436ddaf63
Wessel van Tilburg <wessel.vantilburg@iodigital.com>
2024-02-02 07:32:39 +0100

    add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1 className="text-4xl">Vakantie Ouwe</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
